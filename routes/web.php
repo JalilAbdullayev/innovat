a854a866ba46ac892766c0ab3de635b1921e8e9a
JalilAbdullayev <abdullacelil00@gmail.com>
2024-07-04 15:51:00 +0400

    Install necessary packages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use UniSharp\LaravelFilemanager\Lfm;
 
-Route::get('/', function () {
+Route::get('/', function() {
     return view('welcome');
 });
+
+Route::prefix('admin')->group(function() {
+    Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web', 'auth']], function() {
+        Lfm::routes();
+    });
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
